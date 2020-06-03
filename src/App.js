12041574645df03035a67edcb736fd3c8b8ecc1a
Chris <clenart4@kent.edu>
2020-06-03 15:54:52 -0400

    added example to download data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
 
 function App() {
+
+  // example data
+  var exampleData = {
+    'hotspots': [{
+      'name': 'Victory Bell',
+      'latitude': 41.150186857592914,
+      'longitude': -81.34437203407289
+    }]
+  };
+
+  // we turn that json into a single string
+  var jsonData = JSON.stringify(exampleData);
+  // turn it into a blob object
+  var blob = new Blob([jsonData], {type: "application/json"})
+  // create blob url for the browser to download
+  var url  = URL.createObjectURL(blob);
+
   return (
     <div className="App">
+      <a href={url} download={'markers.json'}>
+        Download File
+      </a>
     </div>
   );
 }
