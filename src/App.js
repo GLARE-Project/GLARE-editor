94c0e0dfa622077267e9fd77d3164f379fcde810
Chris <clenart4@kent.edu>
2020-06-25 13:38:56 -0400

    cleaned up code and added remaining fields

--- src/App.js
+++ src/App.js
@@ -3,25 +3,25 @@ import './App.css';
 import Home from "./pages/home/Home.js";
 import Createnew from "./pages/createnew/Createnew.js";
 import Updateold from "./pages/updateold/Updateold.js";
-import {Switch, Route, NavLink} from 'react-router-dom';
+import { Switch, Route, NavLink } from 'react-router-dom';
 function App() {
-  return(
+  return (
     <div className="app">
       <nav>
-    <h1>GLARE Configuration Editor</h1>
-    <ul>
-    <li><NavLink to='/'>Home</NavLink></li>
-    <li><NavLink to='/createnew'>Create New</NavLink></li>
-    <li><NavLink to='/updateold'>Update Existing</NavLink></li>
-    </ul>
-    </nav>
-    <main>
-      <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/createnew" component={Createnew}/>
-      <Route exact path="/updateold" component={Updateold} />
-      </Switch>
-    </main>
+        <h1>GLARE Configuration Editor</h1>
+        <ul>
+          <li><NavLink to='/'>Home</NavLink></li>
+          <li><NavLink to='/createnew'>Create New</NavLink></li>
+          <li><NavLink to='/updateold'>Update Existing</NavLink></li>
+        </ul>
+      </nav>
+      <main>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/createnew" component={Createnew} />
+          <Route exact path="/updateold" component={Updateold} />
+        </Switch>
+      </main>
     </div>
   )
 }
--- src/pages/createnew/Createnew.js
+++ src/pages/createnew/Createnew.js
@@ -1,77 +1,87 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './createnew.css';
 function Createnew() {
-    const [Answers, setAnswers] = useState({
-        name: null, 
-        latitude: null,
-        longitude: null,
-        image: null,
-        audio: null
-      });
-    
-      const [downloadURL, setURL] = useState(null);
-      
-    
-      // generate the download URL
-      const generateURL = () => {
-        const jsonData = JSON.stringify(Answers);
-        // turn it into a blob object
-        const blob = new Blob([jsonData], {type: "application/json"})
-        // create blob url for the browser to download
-        return URL.createObjectURL(blob);
-      };
-    
-      
-      // if a value is changed it must:
-      const handleChange = (objectName, value) => {
-        // set the new answer value
-        setAnswers({ ...Answers, ...{[objectName]: value} });
-        // regenerate the URL
-        setURL(generateURL());
-      };
-    
-    
-      return (
-        <div className="newConfigMain">
-          <div className="header">
-          <h3>Please feel free to add a new configuration below!</h3>
-            <form >
-                <pre>
-              Name : 
-              <input type="text" placeholder="enter name"
-                value={Answers.name} 
-                onChange={e => handleChange("name", e.target.value)}
-              />
-    
-              <br></br>
-    
-              <textarea name='userConfigData' rows="10" cols="70" 
-                placeholder=" MAP "/>
-              <br></br>
-              <br></br>
-              Latitude : 
-              <input type="number" placeholder="enter latitude"
-              value={Answers.latitude} 
-              onChange={e => handleChange("latitude", e.target.value)}/>
-              Longitude : 
-              <input type="number" placeholder="enter longitude"
-              value={Answers.longitude} 
-              onChange={e => handleChange("longitude", e.target.value)}
-              />
-              <br></br>
-              Image : 
-              <input type="file" placeholder="select image" accept="image/png, image/jpeg"
-              onChange={e => handleChange("image", e.target.value)}></input>
-              
-              Audio :
-             <input type="file" placeholder="select audio" accept="audio/*"
-             onChange={e => handleChange("audio", e.target.value)}></input>
-             <br></br>
-    {downloadURL && (<a href={downloadURL} download="markers.json">Download File</a>)}
-    </pre>
-    </form>
+  const [Answers, setAnswers] = useState({
+    // location related
+    name: null,
+    position: null,
+    latitude: null,
+    longitude: null,
+    // AR related
+    AR_overlay: null,
+    // VR related
+    panorama_image: null,
+    VR_overylay: null,
+    overlay_size: 10,
+    overlay_offset_x: 0,
+    overlay_offset_y: 0,
+    // audio related
+    audio: null
+  });
+
+  const [downloadURL, setURL] = useState(null);
+
+
+  // generate the download URL
+  const generateURL = () => {
+    const jsonData = JSON.stringify(Answers);
+    // turn it into a blob object
+    const blob = new Blob([jsonData], { type: "application/json" })
+    // create blob url for the browser to download
+    return URL.createObjectURL(blob);
+  };
+
+
+  // if a value is changed it must:
+  const handleChange = (objectName, value) => {
+    // set the new answer value
+    setAnswers({ ...Answers, ...{ [objectName]: value } });
+    // regenerate the URL
+    setURL(generateURL());
+  };
+
+  return (
+    <div className="newConfigMain">
+      <div className="header">
+        <h3>Please feel free to add a new configuration below!</h3>
+      </div>
+
+      Name :
+      <input type="text" placeholder="enter name"
+        value={Answers.name}
+        onChange={e => handleChange("name", e.target.value)}
+      />
+      <br />
+
+      <textarea name='userConfigData' rows="10" cols="70"
+        placeholder=" MAP " />
+      <br />
+
+      Latitude :
+      <input type="number" placeholder="enter latitude"
+        value={Answers.latitude}
+        onChange={e => handleChange("latitude", e.target.value)} />
+
+      Longitude :
+      <input type="number" placeholder="enter longitude"
+        value={Answers.longitude}
+        onChange={e => handleChange("longitude", e.target.value)}
+      />
+      <br />
+
+      Image :
+      <input
+        type="file" placeholder="select image" accept="image/png, image/jpeg"
+        onChange={e => handleChange("image", e.target.value)}
+      />
+
+      Audio :
+      <input type="file" placeholder="select audio" accept="audio/*"
+        onChange={e => handleChange("audio", e.target.value)}></input>
+      <br />
+
+      {downloadURL && (<a href={downloadURL} download="markers.json">Download File</a>)}
     </div>
-    </div>
-      );
-    }
+  );
+}
 export default Createnew;
