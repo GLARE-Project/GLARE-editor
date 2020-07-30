b24de9a18de7b8a55b857ecd0e26b15c54119418
Chris <clenart4@kent.edu>
2020-07-30 16:59:16 -0400

    fixed homepage

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -5,9 +5,11 @@ function Home() {
     return (
         <div className="homepage">
             <img src={"/images/ConfigPhone.png"} alt="logo" />
-            <h1>GLARE</h1>
-            <h2>Configuration Editor</h2>
-            <h5> Welcome to the GLARE Configuration Editor! Glare is an Augumented Reality Editor enabling the development of individual experiences regarding humanties and beyond.</h5>
+            <div className={"side-text"}>
+                <h1>GLARE</h1>
+                <h2>Configuration Editor</h2>
+                <h5> Welcome to the GLARE Configuration Editor! Glare is an Augumented Reality Editor enabling the development of individual experiences regarding humanties and beyond.</h5>
+            </div>
         </div>
     )
 };
