412ee6730768731cb855ff139e80048cee3ca530
Neha <nehayala@gmail.com>
2020-07-31 00:19:35 -0400

    rearranged footer content

--- src/App.js
+++ src/App.js
@@ -84,8 +84,9 @@ function App() {
         <footer>
             <ul>
               <li> FAQ | 2017 &copy; Kent State University | Privacy & Terms of Use </li>
-              <li><img  id="logo" src="/images/Kent State Logo.png"/></li>
               <li><img  id="logo" src="/images/NEH_logo.png"/></li>
+              <li><img  id="logo" src="/images/Kent State Logo.png"/></li>
+
             </ul>
         </footer>
       </div>
