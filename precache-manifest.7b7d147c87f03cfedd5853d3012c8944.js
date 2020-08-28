42c6c6f2b40c68cd41d25448d5b0fa5e47df9a34
Chris <clenart4@kent.edu>
2020-08-27 23:58:39 -0400

    Updates

---
+++ precache-manifest.7b7d147c87f03cfedd5853d3012c8944.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0b1bc2a1d739108bea393ccead0cf6d",
+    "url": "/GLARE-editor/index.html"
+  },
+  {
+    "revision": "3a2a0a251546c6344478",
+    "url": "/GLARE-editor/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "60a0a9516fd9ca31b2c3",
+    "url": "/GLARE-editor/static/css/main.3612dd11.chunk.css"
+  },
+  {
+    "revision": "3a2a0a251546c6344478",
+    "url": "/GLARE-editor/static/js/2.ffa48fe5.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/GLARE-editor/static/js/2.ffa48fe5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60a0a9516fd9ca31b2c3",
+    "url": "/GLARE-editor/static/js/main.0741464b.chunk.js"
+  },
+  {
+    "revision": "8ac1fcba9cddf45e3394",
+    "url": "/GLARE-editor/static/js/runtime-main.2e2e8d50.js"
+  }
+]);
