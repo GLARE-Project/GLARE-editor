8b6fec60803ccc36d7900a2742b027041c40567f
Chris <clenart4@kent.edu>
2020-09-03 12:20:47 -0400

    Updates

---
+++ precache-manifest.6cae136d956d393b6c8ff2f131b6e44c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c9b1163aad8a3fe49029e8f8e0e2f25",
+    "url": "/GLARE-editor/index.html"
+  },
+  {
+    "revision": "35468280f263ccf9083e",
+    "url": "/GLARE-editor/static/css/2.61e7bc16.chunk.css"
+  },
+  {
+    "revision": "28f5d059ecae2e7844bf",
+    "url": "/GLARE-editor/static/css/main.83e47cfa.chunk.css"
+  },
+  {
+    "revision": "35468280f263ccf9083e",
+    "url": "/GLARE-editor/static/js/2.3991a6ad.chunk.js"
+  },
+  {
+    "revision": "6791e7fb9584d65ed87d655035c7da95",
+    "url": "/GLARE-editor/static/js/2.3991a6ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28f5d059ecae2e7844bf",
+    "url": "/GLARE-editor/static/js/main.6b035fa4.chunk.js"
+  },
+  {
+    "revision": "8ac1fcba9cddf45e3394",
+    "url": "/GLARE-editor/static/js/runtime-main.2e2e8d50.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/GLARE-editor/precache-manifest.ca2897577fcbe3158e4c764c824a5dba.js"
+  "/GLARE-editor/precache-manifest.6cae136d956d393b6c8ff2f131b6e44c.js"
 );
 
 self.addEventListener('message', (event) => {
